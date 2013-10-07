2340017958a87c2adee1d6532754e79eb5a70d56
Sandro Padin <sandropadin@gmail.com>
2013-10-07 01:00:51 -0500

    Rearranging controllers

--- src/clj/scoreboard_web/core.clj
+++ src/clj/scoreboard_web/core.clj
@@ -1,18 +1,19 @@
 (ns scoreboard-web.core
   (:require
-    [compojure.core :refer [defroutes GET]]
+    [compojure.core :refer [GET defroutes context]]
     [compojure.route :refer [not-found]]
+    [scoreboard-web.home.home-controller :refer [home-controller]]
     [joodo.middleware.view-context :refer [wrap-view-context]]
-    [joodo.views :refer [render-template render-html]]
-    [joodo.controllers :refer [controller-router]]))
+    [joodo.views :refer [render-template]]
+    [ring.util.response :refer [redirect]]))
 
 (defroutes scoreboard-web-routes
-  (GET "/" [] (render-template "home/index"))
-  (controller-router 'scoreboard-web.controller)
-  (not-found (render-template "util/not_found" :template-root "scoreboard_web" :ns `scoreboard-web.util.view-helpers)))
+  (GET "/" [] (redirect "/home"))
+  (context "/home" [] home-controller)
+  (not-found (render-template "util/not_found" :template-root "scoreboard_web" :layout "util/layout" :ns `scoreboard-web.util.view-helpers)))
 
 (def app-handler
   (->
     scoreboard-web-routes
-    (wrap-view-context :template-root "scoreboard_web" :ns `scoreboard-web.util.view-helpers)))
+    (wrap-view-context :template-root "scoreboard_web" :layout "util/layout" :ns `scoreboard-web.util.view-helpers)))
 
---
+++ src/clj/scoreboard_web/home/home_controller.clj
@@ -0,0 +1,6 @@
+(ns scoreboard-web.home.home-controller
+  (:require [compojure.core :refer (GET defroutes)]
+            [joodo.views :refer (render-template)]))
+
+(defroutes home-controller
+  (GET "/" [] (render-template "home/index" :template-root "scoreboard_web" :ns `scoreboard-web.util.view-helpers)))
